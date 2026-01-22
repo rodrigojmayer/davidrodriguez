4b7b2bdc467f218f314fef4eaed38c1831a2bffb
rodrigojmayer <rodrigojmayer@gmail.com>
2026-01-22 18:38:29 -0300

    whatsapp number updated

--- App.tsx
+++ App.tsx
@@ -240,7 +240,7 @@ const App: React.FC = () => {
 
       <div className="fixed bottom-6 right-6 z-50">
         <a 
-          href="https://wa.me/5493415559876" 
+          href="https://wa.me/5493416524309" 
           target="_blank" 
           className="bg-green-500 text-white p-4 rounded-full shadow-2xl hover:bg-green-600 transition-all flex items-center justify-center animate-bounce"
         >
