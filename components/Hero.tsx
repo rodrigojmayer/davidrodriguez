78d103b310c39fa157362e4892d90a21ae2b9b2f
rodrigojmayer <rodrigojmayer@gmail.com>
2026-01-23 09:59:21 -0300

    hero buttons accomodation for different screen sizes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,11 @@ const Hero: React.FC = () => {
 
       <div className="container mx-auto px-4 relative z-10">
         <div className="max-w-3xl animate-slideUp">
-          <h2 className="text-blue-500 font-bold uppercase tracking-widest mb-4">Especialistas en Estética Automotriz</h2>
+          <h2 className="text-blue-500 font-bold uppercase tracking-widest 
+            mt-20 mb-0
+            md:mt-0 md:mb-4"
+          >
+            Especialistas en Estética Automotriz</h2>
           <h1 className="text-5xl md:text-7xl font-bold text-white leading-tight mb-6">
             Chapería y Pintura <br />
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600">Alta Gama</span>
@@ -32,13 +36,13 @@ const Hero: React.FC = () => {
           <div className="flex flex-col sm:flex-row gap-4">
             <a 
               href="#contacto" 
-              className="bg-blue-600 hover:bg-blue-700 text-white px-10 py-4 rounded-full font-bold text-lg text-center transition-all shadow-xl hover:shadow-blue-500/20"
+              className="hidden md:inline-block bg-blue-600 hover:bg-blue-700 text-white px-10 py-4 rounded-full font-bold text-lg text-center transition-all shadow-xl hover:shadow-blue-500/20 "
             >
               CONTACTANOS
             </a>
             <a 
               href="#galeria" 
-              className="bg-white/10 hover:bg-white/20 text-white backdrop-blur-md px-10 py-4 rounded-full font-bold text-lg text-center border border-white/20 transition-all"
+              className="mb-16 md:mb-0 bg-white/10 hover:bg-white/20 text-white backdrop-blur-md px-10 py-4 rounded-full font-bold text-lg text-center border border-white/20 transition-all"
             >
               VER TRABAJOS
             </a>
@@ -46,10 +50,11 @@ const Hero: React.FC = () => {
         </div>
       </div>
 
-      {/* Scroll indicator */}
-      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
-        <span className="text-white/50 text-xs uppercase tracking-widest mb-2">Deslizar</span>
-        <div className="w-1 h-8 bg-gradient-to-b from-blue-500 to-transparent rounded-full" />
+      {/* Scroll indicator */}      
+     <div className="absolute bottom-[0px] inset-x-0 flex flex-col items-center animate-bounce ">
+      <span className="text-white/50 text-xs uppercase tracking-widest mb-2 animate-bounce">
+        Deslizar</span>
+      <div className="animate-bounce w-1 h-8 bg-gradient-to-b from-blue-500 to-transparent rounded-full" />
       </div>
     </div>
   );
