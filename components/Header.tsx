e657cc46af135c06e11d9d375d39376aac128481
rodrigojmayer <rodrigojmayer@gmail.com>
2026-01-24 18:53:14 -0300

    added favicom

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,6 +26,7 @@ const Header: React.FC = () => {
         {/* Logo */}
         <a href="#inicio" className="flex items-center space-x-2">
           <div className="w-14 h-10 bg-blue-600 rounded flex items-center justify-center font-bold text-white text-xl italic">TRC</div>
+          {/* <img src="../imgs/favicom7.png" className="w-10 h-10" alt="TRC"/> */}
           <span className={`text-2xl font-bold tracking-tight ${isScrolled ? 'text-white' : 'text-white'}`}>
             Taller Rodriguez Camiones
           </span>
